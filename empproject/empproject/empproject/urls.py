76e64949ec682513cfe8546d3b0d7ead5d55cf50
Data Science Vibes <tanwarsakshi1717157@gmail.com>
2026-04-07 11:18:35 +0530

    Delete empproject directory

--- empproject/empproject/empproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp.views import createEmp,show
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('emp', createEmp),
-    path('show/', show),
-]
--- empproject/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Employee(models.Model):
-    eid=models.CharField(primary_key=True,max_length=10)
-    ename=models.CharField(max_length=100)
-    email=models.EmailField()
-    phone=models.CharField(max_length=15)
